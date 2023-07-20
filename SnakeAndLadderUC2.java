ca00a516c5d9c2a9a2eb9fb6cedddf203dc15605
rohit789@ <princeshmr27@gmail.com>
2023-07-20 23:38:06 +0530

    The Player rolls the die to get a number between 1 to 6

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,11 @@
+class SnakeAndLadderUC2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Single player start position 0");
+		System.out.println("Welcome to Snake And Ladder game");
+
+		int dieValue=(int)((Math.random()*100)%6)+1;
+		System.out.println(dieValue);
+	}
+}
